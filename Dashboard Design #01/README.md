6986fdc60224113e8fa394dc24dc76d06f892af6
Reza Mehdikhanlou <mehdikhanloureza@gmail.com>
2023-12-21 04:37:35 +0330

    Adding Dashboard Design #01

---
+++ Dashboard Design #01/README.md
@@ -0,0 +1,10 @@
+# Responsive Dashboard Design #01
+In this tutorial ([Open in Youtube](https://youtu.be/KMWZB-MD8LQ)),  I'm going to show you how to use modern HTML, CSS, and JavaScript to create a completely responsive dashboard with mobile menu functionality. We'll be using CSS Grid, CSS Flexbox, Media queries for our responsive design, and CSS  transitions for some cool animation effects. This dashboard design is responsive in all devices!
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot1](screenshot-1.png)
+
+### Mobile Screenshot
+![screenshot2](screenshot-2.png)
---
+++ Dashboard Design #01/script.js
@@ -0,0 +1,26 @@
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sidebarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+
+menuOpen.addEventListener('click', () => {
+    sideBar.style.top = '0';
+});
+
+menuClose.addEventListener('click', () => {
+    sideBar.style.top = '-60vh';
+});
+
+let activeItem = sidebarItems[0];
+
+sidebarItems.forEach(element => {
+    element.addEventListener('click', () => {
+        if (activeItem) {
+            activeItem.removeAttribute('id');
+        }
+
+        element.setAttribute('id', 'active');
+        activeItem = element;
+
+    });
+});
