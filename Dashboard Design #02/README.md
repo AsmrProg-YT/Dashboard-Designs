8320c6acb0c05561a204a4c29664639feca1696b
Reza Mehdikhanlou <rezamehdikhanlou@gmail.com>
2024-07-12 21:13:43 +0330

    Adding Dashboard #02 Source

---
+++ Dashboard Design #02/README.md
@@ -0,0 +1,21 @@
+# Responsive Dashboard Design #02
+In this tutorial ([Open in Youtube](https://youtu.be/YthqiuzEXBs)), 🎨 We’ll begin by structuring the layout with HTML, then styling it with CSS for a modern, sleek look. Finally, we'll incorporate JavaScript to add interactive features like displaying activity charts!
+
+What You’ll Learn:
+- Structuring Your HTML: Laying the foundation for your dashboard with well-organized HTML.
+- Styling with CSS: Designing a modern and attractive interface with CSS techniques for layout, colors, and fonts.
+- Adding Interactivity with JavaScript: Making the dashboard dynamic and user-friendly.
+- Creating a Responsive Design: Ensuring your dashboard looks great on desktops, tablets, and smartphones.
+- Custom Components: Building elements like progress bars, charts, and interactive schedules.
+
+Key Features:
+- Course Progress Overview: Visualize your progress in different courses with clear, stylish progress bars.
+- Learning Progress Chart: Track your learning progress with an interactive graph that shows your current GPA and class average GPA over time.
+- Weekly Activity Tracker: Monitor your activity throughout the week with a sleek bar chart.
+- Recommendations: Get personalized course recommendations based on your activity and progress.
+- Popular Resources: Access popular resources and podcasts to enhance your learning experience.
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot1](screenshot.png)
---
+++ Dashboard Design #02/script.js
@@ -0,0 +1,95 @@
+const ctx = document.querySelector('.activity-chart');
+const ctx2 = document.querySelector('.prog-chart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [{
+            label: 'Time',
+            data: [8, 6, 7, 6, 10, 8, 4],
+            backgroundColor: '#1e293b',
+            borderWidth: 3,
+            borderRadius: 6,
+            hoverBackgroundColor: '#60a5fa'
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                border: {
+                    display: true
+                },
+                grid: {
+                    display: true,
+                    color: '#1e293b'
+                }
+            },
+            y: {
+                ticks: {
+                    display: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutQuad',
+        }
+    }
+});
+
+new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        datasets: [{
+            label: 'Class GPA',
+            data: [6, 10, 8, 14, 6, 7, 4],
+            borderColor: '#0891b2',
+            tension: 0.4
+        },
+        {
+            label: 'Aver GPA',
+            data: [8, 6, 7, 6, 11, 8, 10],
+            borderColor: '#ca8a04',
+            tension: 0.4
+        }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                ticks: {
+                    display: false
+                },
+                border: {
+                    display: false,
+                    dash: [5, 5]
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutQuad',
+        }
+    }
+});
